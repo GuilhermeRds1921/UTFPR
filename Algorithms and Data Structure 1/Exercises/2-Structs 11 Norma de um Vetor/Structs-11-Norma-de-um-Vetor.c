2ffac9fc6ba506badb9594e50c14e3f3944dcd22
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-20 15:17:13 -0300

    adicionei até o 3

---
+++ Algorithms and Data Structure 1/Exercises/2-Structs 11 Norma de um Vetor/Structs-11-Norma-de-um-Vetor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+typedef struct vetor_t{
+    float x;
+    float y;
+    float norm;
+}vetor_t;
+
+vetor_t calculaNorma(vetor_t vetor){
+    vetor.norm = pow(vetor.x, 2) + pow(vetor.y, 2);
+    return vetor;
+}
+
+int main(){
+    vetor_t vetor, print;
+
+    int num, i;
+    scanf("%d", &num);
+
+    for(i=0; i<num; i++){
+        scanf("%f %f", &vetor.x, &vetor.y);
+        print = calculaNorma(vetor);
+        printf("Norma = %.1f\n", sqrt(print.norm));
+    }
+  return 0;
+}
---
+++ Algorithms and Data Structure 1/Exercises/3-Eletrodomésticos (structs)/Eletrodomésticos-(structs).c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct aparelhos{
+    char nome[15];
+    int potencia;
+    float horas;
+};
+int main() {
+
+    int num, i, dias, total=0;
+
+    scanf("%d", &num);
+    struct aparelhos apr[num];
+    float cada[num];
+
+    for(i=0; i<num; i++){
+        scanf("%s", &apr[i].nome);
+        scanf("%d", &apr[i].potencia);
+        scanf("%f", &apr[i].horas);
+        cada[i] = apr[i].potencia*apr[i].horas;
+        total += apr[i].potencia*apr[i].horas;
+    }
+    scanf("%d", &dias);
+
+    printf("%d\n", total*dias);
+    for(i=0; i<num; i++){
+        printf("%s %.1f%\n",apr[i].nome, (cada[i]/total)*100);
+    }
+
+    return 0;
+}

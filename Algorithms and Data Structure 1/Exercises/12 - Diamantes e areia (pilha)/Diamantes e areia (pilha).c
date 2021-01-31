bafa7f73739082e262beaf80dfa863380c71e36f
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-01-31 20:11:17 -0300

    sintaxe

--- Algorithms and Data Structure 1/Exercises/12 - Diamantes e areia (pilha)/Diamantes e areia (pilha).c
+++ Algorithms and Data Structure 1/Exercises/12 - Diamantes e areia (pilha)/Diamantes e areia (pilha).c
@@ -21,7 +21,7 @@ struct pilha{
 Pilha* criar(){
     Pilha* pi = (Pilha*)malloc(sizeof(Pilha));
     pi->qtd = 0;
-    pi->dim=0;
+    pi->dim = 0;
     return pi;
 }
 
@@ -29,7 +29,7 @@ void liberar(Pilha* pi){
     free(pi);
 }
 int inserir(Pilha* pi, struct minerar mi){
-    if(pi ==NULL || pi->qtd == Max){
+    if(pi == NULL || pi->qtd == Max){
         return 0;
     }
     pi->dados[pi->qtd] = mi;
@@ -38,26 +38,26 @@ int inserir(Pilha* pi, struct minerar mi){
 }
 int buscar(Pilha* pi, Pilha* di){
     int tam, i=0;
-    if(pi==NULL ||di ==NULL || pi->qtd == 0){
+    if(pi == NULL ||di ==NULL || pi->qtd == 0){
         return 0;
     }
      tam = strlen(pi->dados[pi->qtd-1].mine);
     do{
-        if(pi->dados[pi->qtd-1].mine[i]=='<'){
+        if(pi->dados[pi->qtd - 1].mine[i] == '<'){
             di->qtd++;
         }
-        if(di->qtd>0 && pi->dados[pi->qtd-1].mine[i]=='>')
+        if(di->qtd > 0 && pi->dados[pi->qtd - 1].mine[i] == '>')
         {
             di->dim++;
             di->qtd--;
         }
         i++;
-    }while(i<tam);
+    }while(i < tam);
 
     return 1;
 }
 int main() {
-    int casos, j=0;
+    int casos, j = 0;
     scanf("%d\n", &casos);
     Pilha* pi;
     Pilha* di;
@@ -71,6 +71,6 @@ int main() {
         buscar(pi, di);
         printf("%d\n", di->dim);
         j++;
-    }while(j<casos);
+    }while(j < casos);
     return 0;
 }

7d5350389c13ed49367d9f8133bd174bdba06880
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-02-13 13:35:56 -0300

    ex 24

---
+++ Algorithms and Data Structure 1/Exercises/24-Máximo divisor comum (recursão)/Máximo divisor comum (recursão).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int recursao(int n1, int n2){
+    if(n1==n2){
+        return n1;
+    }
+    if(n1<n2){
+        return recursao(n2, n1);
+    }
+    else{
+        return recursao(n1-n2, n2);
+    }
+}
+int main() {
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    printf("%d",recursao(n1, n2));
+    return 0;
+}

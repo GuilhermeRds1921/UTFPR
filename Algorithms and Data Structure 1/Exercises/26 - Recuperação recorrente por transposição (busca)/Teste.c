3d361166d665939e0157186dcb39da7595eef203
GuilhermeRdS1921 <guilhermerodrigues1921@gmail.com>
2021-02-18 13:45:55 -0300

    Sintaxe

--- Algorithms and Data Structure 1/Exercises/26 - Recuperação recorrente por transposição (busca)/Teste.c
+++ Algorithms and Data Structure 1/Exercises/26 - Recuperação recorrente por transposição (busca)/Teste.c
@@ -5,9 +5,9 @@
 
 int Trans(int *V, int tam, int key){
     int i, aux;
-    for(i=0; i<tam; i++){
+    for(i = 0; i < tam; i++){
         if(key == V[i]){
-            if(V[0]==key){
+            if(V[0] == key){
                 return i;
             }
             else{
@@ -24,17 +24,17 @@ int main() {
     int i, tam, casos, key;
     scanf("%d", &tam);
     int v[tam];
-    for(i=0; i<tam; i++){
+    for(i = 0; i < tam; i++){
         scanf("%d", &v[i]);
     }
     int *V = v;
     scanf("%d", &casos);
 
-    for(i=0; i<casos; i++){
+    for(i = 0; i < casos; i++){
         scanf("%d", &key);
         Trans(V, tam, key);
     }
-    for(int j=0; j<tam; j++){
+    for(int j = 0; j < tam; j++){
          printf("%d ", V[j]);
     }
     return 0;
